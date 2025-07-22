8c3d878a8e3862adbb9780d9af033da73f04597e
huchenfan <15779163358@163.com>
2025-07-22 09:40:06 +0800

    7.21更新

---
+++ Leetcode/C/leetcode07/leetcode_07_1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int reverse(int x) {
+    long result = 0;
+
+    while(x){
+        result = result * 10 + x % 10;
+        x /= 10;
+    }
+
+    return (result > INT_MIN && result <INT_MAX) ? result : 0;
+}
+
+int main(void) {
+    int num = -89562;
+    int num_ = reverse(num);
+    printf("%d", num_);
+    return 0;
+}
